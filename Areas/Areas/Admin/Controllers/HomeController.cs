8d4755efa3861c4a91b25f143b90c6d2078e7fae
Coskun Canli <coskun.canli@gmail.com>
2023-03-21 21:14:16 +0300

    logput

--- Areas/Areas/Admin/Controllers/HomeController.cs
+++ Areas/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Areas.Areas.Admin.Controllers
@@ -9,7 +11,17 @@ namespace Areas.Areas.Admin.Controllers
     {
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.Username = User.Identity.Name;
+            }
             return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
