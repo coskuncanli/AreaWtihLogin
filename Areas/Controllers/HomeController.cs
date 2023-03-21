1568bb70f1316420fd691156b1e1881125b95a81
coskuncanli <coskun.canli@gmail.com>
2023-03-21 16:53:54 +0300

    start

---
+++ Areas/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Areas.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Areas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            User? x = UserInit.Init().FirstOrDefault(x => x.Username == user.Username && x.Password == user.Password);
+            if (x != null)
+            {
+                var scheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                var Claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+
+                var claimsIdentity = new ClaimsIdentity(Claims, scheme);
+                var authProoerties = new AuthenticationProperties();
+
+                await HttpContext.SignInAsync(scheme, new ClaimsPrincipal(claimsIdentity), authProoerties);
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            else
+            {
+                ViewBag.LoginError = "Hatalı kullanıcı adı ya da şifre";
+            }
+            return View(); ;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Areas/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Areas.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Remember { get; set; }
+    }
+
+    public class UserInit
+    {
+        public static List<User> Init()
+        {
+            return new List<User>()
+            {
+                new User(){ Username = "Ali", Password = "123456", Remember = "on"},
+                new User(){ Username = "Can", Password = "123456", Remember = "off"},
+                new User(){ Username = "Mehmet", Password = "123456", Remember = "off"},
+                new User(){ Username = "Mustafa", Password = "123456", Remember = "on"}
+            };
+        }
+    }
+}
